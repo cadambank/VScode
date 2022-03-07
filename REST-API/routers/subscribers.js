fea4e1dc32ee888fb1fa569930fd35e46fa87c76
cadambank <cadamban.k@gmail.com>
2022-03-08 02:59:38 +0530

    Learning REST-API

--- REST-API/routers/subscribers.js
+++ REST-API/routers/subscribers.js
@@ -1,26 +1,76 @@
 const express = require('express');
+const subscribers = require('../models/subscribers');
 const router = express.Router();
+const Subscriber = require('../models/subscribers');
 
 module.exports = router;
 
 //Getting All
-router.get('/',(req,res)=>{
-    res.send('Hello World');
+router.get('/',async (req,res)=>{
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    }
+    catch(error) {
+        res.status(500).json({message : error.message})
+    }
 })
 //Getting One
-router.get('/:id',(req,res)=>{
-        
+router.get('/:id',getSubscriber,(req,res)=>{
+    res.send(res.subscriber);
+    // console.log(res);
 })
 //Creating One
-router.post('/',(req,res)=>{
-    
+router.post('/',async (req,res)=>{
+    const subscriber = new Subscriber({
+        name : req.body.name,
+        subscriberToChannel : req.body.subscriberToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save();
+        res.status(201).json(newSubscriber);
+    } catch (error){
+        res.status(400).json({message:error.message});
+    }
 })
 //Updating One
-router.patch('/',(req,res)=>{
-    
+router.patch('/:id',getSubscriber, async (req,res)=>{
+    if(req.body.name != null){
+        res.subscriber.name = req.body.name
+    }
+    if(req.body.subscriberToChannel != null){
+        res.subscriber.subscriberToChannel = req.body.subscriberToChannel
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber);
+    }
+    catch (err) {
+        res.status(400).json({message:err.message})
+    }
 })
 //Deleting one
-router.delete('/:id',(req,res)=>{
-    
+router.delete('/:id',getSubscriber,async (req,res)=>{
+    try {
+        console.log(res.subscriber);
+        await res.subscriber.remove();
+        res.json({message:"Subscriber Deleted"})
+    } catch(err) {
+        res.status(500).json({message:err.message})
+    }
 })
-
+ 
+async function getSubscriber(req,res,next){
+    let subscriber;
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({message: 'cannot find subscriber'})
+            console.log("Data Not Available");
+        }
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+    res.subscriber = subscriber;
+    next();
+}
